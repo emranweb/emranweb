40c30e6792f8ca9cfb1b6401fc192a06250f9908
Emran <emranbd77@gmail.com>
2017-11-05 21:36:42 +0600

    emranweb portfolio website all essential file include

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  'use strict';
+  $('#html-bar').barfiller({
+   barColor:"#484848",
+  });
+  $('#css-bar').barfiller({
+    barColor:"#484848",
+  });
+  $('#jquery-bar').barfiller({
+    barColor:"#484848",
+  });
+  $('#javascript-bar').barfiller({
+    barColor:"#484848",
+  });
+  $('#bootstrap-bar').barfiller({
+    barColor:"#484848",
+  });
+});
