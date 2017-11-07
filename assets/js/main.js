690eb961b6efca1c9c5577159bd3d5705f1af646
Emran <emranbd77@gmail.com>
2017-11-08 00:16:17 +0600

    desktop version compleate need to work in portfolio area

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   'use strict';
   $('#html-bar').barfiller({
-   barColor:"#484848",
+    barColor: "#484848",
   });
   $('#css-bar').barfiller({
-    barColor:"#484848",
+    barColor: "#484848",
   });
   $('#jquery-bar').barfiller({
-    barColor:"#484848",
+    barColor: "#484848",
   });
   $('#javascript-bar').barfiller({
-    barColor:"#484848",
+    barColor: "#484848",
   });
   $('#bootstrap-bar').barfiller({
-    barColor:"#484848",
+    barColor: "#484848",
   });
 
-//---------------------------
-// init Isotope
-$('.grid').isotope({
-  layoutMode: 'fitRows',
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  masonry: {
-    columnWidth: '.grid-sizer',
-  }
+  //---------------------------
+  // init Isotope
+  $('.grid').isotope({
+    layoutMode: 'fitRows',
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer',
+    }
+  });
+  // filter items on button click
+  $('.filter-button-group').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    $('.grid').isotope({
+      filter: filterValue
+    });
+  });
+
+
+
+
+  $('.carousel').carousel({
+    interval: 3000
+  });
+
 });
-// filter items on button click
-$('.filter-button-group').on( 'click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $('.grid').isotope({ filter: filterValue });
-});
-
-
-
-
-$('.carousel').carousel({
-  interval: 3000
-});
-
-});
-
