77b6672c905d894c7127470d9e5eda2bb4ba57af
Emran <emranbd77@gmail.com>
2017-11-27 23:27:14 +0600

    Deleate bootstrap sass and update header nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 
   'use strict';
+
+  //progressbar
   $('#html-bar').barfiller({
     barColor: "#484848",
   });
@@ -19,6 +21,7 @@ $(document).ready(function () {
 
   //---------------------------
 
+  //Isotope  Plugin
   $('.grid').isotope({
     itemSelector: '.grid-item',
     percentPosition: true,
@@ -29,15 +32,44 @@ $(document).ready(function () {
   });
 
   // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
+  $('.filter-button-group').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
-    $('.grid').isotope({ filter: filterValue });
+    $('.grid').isotope({
+      filter: filterValue
+    });
   });
 
 
-
+  // Bootstrap Carousel interval
   $('.carousel').carousel({
     interval: 3000
   });
+
+  //Scroll top 
+  $(document).on("scroll", function () {
+    if ($(window).scrollTop() > 50) {
+      $(".navbar").addClass("bg-change")
+    } else {
+      $(".navbar").removeClass('bg-change')
+    }
+  });
+
+
+  //wow animation
+
+  var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  });
+  wow.init();
+
 
 });
