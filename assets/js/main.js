da5ee417e26941a9ba0f039377ac83626324992a
emranweb <emranbd77@gmail.com>
2018-02-18 17:59:02 +0600

    scrollspy animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,4 +72,27 @@ $(document).ready(function () {
   wow.init();
 
 
+
+  //bootstrap scroll 
+
+  $(".navbar-nav li a[href^='#']").on('click', function(e) {
+    // prevent default anchor click behavior
+    e.preventDefault();
+ 
+    // store hash
+    var hash = this.hash;
+ 
+    // animate
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+      },700, function(){
+ 
+        // when done, add hash to url
+        // (default click behaviour)
+        window.location.hash = hash;
+      });
+ 
+ });
+
+
 });
