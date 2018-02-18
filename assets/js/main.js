a7dba8aca9c04a792094763f0897441a26512104
emranweb <emranbd77@gmail.com>
2018-02-18 23:03:33 +0600

    add image in portfolio area

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,5 +94,25 @@ $(document).ready(function () {
  
  });
 
+ $(".header-btn").on('click', function(e) {
+  // prevent default anchor click behavior
+  e.preventDefault();
+
+  // store hash
+  var hash = this.hash;
+
+  // animate
+  $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    },300, function(){
+
+      // when done, add hash to url
+      // (default click behaviour)
+      window.location.hash = hash;
+    });
+
+});
+
+
 
 });
